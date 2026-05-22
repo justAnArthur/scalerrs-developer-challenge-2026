513ebf7f452243fe5084bec3e2d9040bd6f064d4
justAnArthur <kozubov.arthur@gmail.com>
2026-05-22 17:39:19 +0200

    chore: fetch Google Doc content and write to index.html

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+const googleDocId = Bun.env.GOOGLE_DOC_ID!
+
+async function fetchGoogleDoc(docId: string) {
+  const url = `https://docs.google.com/document/d/${docId}/export?format=html`
+  const response = await fetch(url, {
+    headers: new Headers({ 'Content-Type': 'application/json' })
+  })
+  if (!response.ok) throw new Error(`Failed to fetch doc: ${response.status}`)
+  return response.text()
+}
+
+const googleDoc = await fetchGoogleDoc(googleDocId)
+
+console.log('googleDoc', googleDoc)
+
+await Bun.write('./index.html', googleDoc)
